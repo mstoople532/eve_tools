69c5ad369aaf7268ebb5eee44bbba14ae7ba6278
Molly <mstoople@gmail.com>
2020-06-28 13:46:37 -0400

    Uses redirect instead of rerouting controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'fit_auditor#index'
+  root to: redirect(path: '/fit_auditor', status: 302)
 
   resources :fit_auditor, only: [:index]
   post :fit_auditor, to: 'fit_auditor#audit'
