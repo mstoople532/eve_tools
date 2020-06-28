1db3fdd5aeb93eb7e3af7e850ba9d063bfebe0e6
Molly <mstoople@gmail.com>
2020-06-28 14:20:52 -0400

    fixes bug where buy doesn't populate properly

--- app/services/fit_comparer.rb
+++ app/services/fit_comparer.rb
@@ -14,15 +14,9 @@ class FitComparer
     new_tally = new_fitting.tally
     old_tally = old_fitting.tally
 
-    old_tally.map do |item, count|
-      if new_tally.include?(item)
-        new_count = new_tally.fetch(item)
-        amount_needed = new_count - count
-        difference[item] = amount_needed
-      else
-        difference[item] = count
-      end
-    end
+    compare_sell_and_keep(old_tally, new_tally)
+    compare_buy(old_tally, new_tally)
+
     respond
   end
 
@@ -36,6 +30,27 @@ class FitComparer
   
   private
 
+  def compare_sell_and_keep(old_tally, new_tally)
+    old_tally.map do |item, count|
+      if new_tally.include?(item)
+        new_count = new_tally.fetch(item)
+        amount_needed = new_count - count
+        difference[item] = amount_needed
+      else
+        new_count = 0
+        amount_needed = new_count - count
+        difference[item] = amount_needed
+      end
+    end
+  end
+
+  def compare_buy(old_tally, new_tally)
+    new_tally.filter_map do |item, count|
+      unless old_tally.include?(item)
+         difference[item] = new_tally.fetch(item)
+      end
+    end
+  end
   def calculate_to_sell(list)
     sell = list.select{ |k,v| v < 0 }
     sell.transform_values(&:abs) 
