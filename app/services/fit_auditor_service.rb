20bea18a4844ee6d84901c97f8cf8c136d6f07ea
Molly <mstoople@gmail.com>
2020-08-22 20:18:58 -0400

    style changes

--- app/services/fit_auditor_service.rb
+++ app/services/fit_auditor_service.rb
@@ -9,7 +9,11 @@ class FitAuditorService
   def call
     old = parse_fitting(old_fitting)
     new = parse_fitting(new_fitting)
-    FitComparer.new(old_fitting: old[:parsed_fitting], new_fitting: new[:parsed_fitting]).call
+
+    FitComparer.new(
+      old_fitting: old[:parsed_fitting], 
+      new_fitting: new[:parsed_fitting]
+    ).call
   end
 
   def parse_fitting(fitting)
